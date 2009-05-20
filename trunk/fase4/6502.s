09f5a69d4df56ffde201dce786dae7d28ff2768f
Sebastiaan.oosterwaal <Sebastiaan.oosterwaal@f14e8f46-2413-11de-8d76-854d91f79a93>
2009-05-20 07:43:58 +0000

    6502.s fixes

--- trunk/fase4/6502.s
+++ trunk/fase4/6502.s
@@ -44,7 +44,7 @@ start:
 
 	movl $0, %eax			
 
-	memloop:				#Initialize the memory to 0
+	memloop:			#Initialize the memory to 0
 	movl $0, MEM(%eax)		
 	addl $4, %eax
 	
@@ -69,18 +69,16 @@ start:
 ###### initpc: initalise the program counter #####
 ##################################################
 initpc:
-	
-	movl $0, %eax			#Reset eax
-	mov $0xfffd, %ax		#Move the memory location of the higher address of PC to ax
-	
-	movl $0, %ebx			#Reset ebx
-	movb MEM(%eax), %bh		#Move the higher address of PC to bh
+		
+	movl $0xfffd, %eax		#Move the memory location of the higher address of PC to ax	
+
+	movzbl MEM(%eax), %ebx		#Move the higher address of PC to bh
 		
 	mov $0xfffc, %ax		#Move the memory location of the lower address of PC to ax
 		
 	movb MEM(%eax), %bl		#Move the lower address of PC to bl
 
-	mov %bx, PC			#Sets the PC
+	movw %bx, PC			#Sets the PC
 
 	ret				#Return from subroutine
 
